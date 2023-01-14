0e54d5f44fd4061a501dd53b02bcb96aeb716892
Enock <enock.mokua@student.moringaschool.com>
2023-01-14 09:44:00 +0300

    create respond with method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,11 @@
 class SessionsController < ApplicationController
+  Devise::SessionsController
 
-  
+  respond_to :json
+
+  private
+
+  def respond_with(response, _opts = {})
+    render json: { message: "You are logged in." }, status: :ok
+  end
 end
